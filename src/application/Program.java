a4567b7f6f787de7c22c7e3835c63c0b43458caa
Olavo Moreira <olavomoreira99@gmail.com>
2024-03-14 21:59:29 -0300

    Very bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Check-in date: ");
+        Date checkIn = sdf.parse(sc.next());
+
+        System.out.print("Check-out date: ");
+        Date checkOut = sdf.parse(sc.next());
+
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date.");
+        }
+        else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: "  + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+
+            System.out.print("Check-in date: ");
+            checkIn = sdf.parse(sc.next());
+
+            System.out.print("Check-out date: ");
+            checkOut = sdf.parse(sc.next());
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println(reservation);
+        }
+        sc.close();
+    }
+}
